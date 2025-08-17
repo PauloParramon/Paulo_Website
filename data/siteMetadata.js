f9355693e6ae1cb073eecd7bd98c68b300c413ba
PauloParramon <pauloparramon@gmail.com>
2025-08-17 12:38:18 -0700

    remove header label

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Paulo Parramon Website',
+  author: 'Paulo Parramon',
+  headerTitle: 'About Me',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
